5c7016a6f2e46242b28dde040b33dfdcb536f1a3
Aleks <Aleks.Simanova@gmail.com>
2025-02-20 01:26:41 +0300

    2.0

--- ui_pipeline.groovy
+++ ui_pipeline.groovy
@@ -1,34 +1,37 @@
-node("maven") {
-    timeout(300) {
-        wrap([$class: 'BuildUser']) {
-            currentBuild.description = "Owner: ${BUILD_USER}"
-
-            yaml_params = readYaml text: $YAML_CONFIG
-        }
-
-        stage("Checkout") {
+timeout(60) {
+    node('maven-slave') {
+        stage('Checkout') {
             checkout scm
+            // git branch: "$BRANCH", credentialsId: 'jenkins', url: 'git@github.com:saint88/jenkins.git'
         }
+        stage('Run tests') {
+            def jobs = [:]
 
-        stage("Prepare parameters") {
-            sh "echo BROWSER=${yaml_params['BROWSER']} > ./.env"
-            sh "echo VERSION=$BROWSER_VERSION >> ./.env"
-        }
+            def runnerJobs = "$TEST_TYPE".split(",")
 
-        stage("Running tests") {
-            sh "docker run --env-file ./.env -v /root/allure:/root/allure -t ui_tests:1.0.0"
-        }
-
-        stage("Publish allure report") {
-            withAllureUpload(indexExistingFiles: true, name: '${JOB_NAME}-${STAND_NAME}-#${BUILD_NUMBER}', projectId: '83', serverId: 'default', tags: "api, regular, automated, ${JOB_NAME}", results: [[path: 'allure-results']]) {
-                allure([
-                        includeProperties: false,
-                        jdk: '',
-                        properties: [],
-                        reportBuildPolicy: 'ALWAYS',
-                        results: [[path: 'allure-results']]
-                ])
+            jobs['ui_tests'] = {
+                node('maven-slave') {
+                    stage('Ui tests on chrome') {
+                        if('ui' in runnerJobs) {
+                            catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                                build(job: 'ui-tests',
+                                        parameters: [
+                                                string(name: 'BRANCH', value: BRANCH),
+                                                string(name: 'BASE_URL', value: BASE_URL),
+                                                string(name: 'BROWSER', value: BROWSER),
+                                                string(name: 'BROWSER_VERSION', value: BROWSER_VERSION),
+                                                string(name: 'GRID_URL', value: GRID_URL)
+                                        ])
+                            }
+                        } else {
+                            echo 'Skipping stage...'
+                            Utils.markStageSkippedForConditional('keystone api tests')
+                        }
+                    }
+                }
             }
+
+            parallel jobs
         }
     }
 }
