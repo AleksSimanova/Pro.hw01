38b65b8613abc94e654d60d82ba8ffdeb4953eb1
Aleks <Aleks.Simanova@gmail.com>
2025-02-20 01:04:18 +0300

    groovy

---
+++ ui_pipeline.groovy
@@ -0,0 +1,35 @@
+node("maven") {
+    timeout(300) {
+        wrap([$class: 'BuildUser']) {
+            currentBuild.description = "Owner: ${BUILD_USER}"
+
+            yaml_params = readYaml text: $YAML_CONFIG
+        }
+
+        stage("Checkout") {
+            checkout scm
+        }
+
+        stage("Prepare parameters") {
+            sh "echo BROWSER=${yaml_params['BROWSER']} > ./.env"
+            sh "echo VERSION=$BROWSER_VERSION >> ./.env"
+        }
+
+        stage("Running tests") {
+            sh "docker run --env-file ./.env -v /root/allure:/root/allure -t ui_tests:1.0.0"
+        }
+
+        stage("Publish allure report") {
+            withAllureUpload(indexExistingFiles: true, name: '${JOB_NAME}-${STAND_NAME}-#${BUILD_NUMBER}', projectId: '83', serverId: 'default', tags: "api, regular, automated, ${JOB_NAME}", results: [[path: 'allure-results']]) {
+                allure([
+                        includeProperties: false,
+                        jdk: '',
+                        properties: [],
+                        reportBuildPolicy: 'ALWAYS',
+                        results: [[path: 'allure-results']]
+                ])
+            }
+        }
+    }
+}
+
